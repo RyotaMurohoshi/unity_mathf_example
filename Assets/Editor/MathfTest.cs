006f49baa90cb3b1f6d5116003bbd6f996751c82
Ryota Murohoshi <ryotamurohoshi@gmail.com>
2016-02-02 01:22:42 +0900

    added Clamp

--- Assets/Editor/MathfTest.cs
+++ Assets/Editor/MathfTest.cs
@@ -32,6 +32,37 @@ public class MathfTest
 	}
 
 	[Test]
+	public void ClampIntTest ()
+	{
+		//Mathf.Clamp (value, min, max);
+		Assert.That (Mathf.Clamp (0, -1, 2), Is.EqualTo (0));
+		Assert.That (Mathf.Clamp (3, -1, 2), Is.EqualTo (2));
+		Assert.That (Mathf.Clamp (-2, -1, 2), Is.EqualTo (-1));
+
+		// iregular args
+		// We shoul with named arg?
+		Assert.That (Mathf.Clamp (0, 1, -1), Is.EqualTo (1));
+		Assert.That (Mathf.Clamp (2, 1, -1), Is.EqualTo (-1));
+		Assert.That (Mathf.Clamp (-2, 1, -1), Is.EqualTo (1));
+	}
+
+	[Test]
+	public void ClampFloatTest ()
+	{
+		//Mathf.Clamp (value, min, max);
+		Assert.That (Mathf.Clamp (0.5F, -1.0F, 1.0F), Is.EqualTo (0.5F));
+		Assert.That (Mathf.Clamp (2.5F, -1.0F, 2.0F), Is.EqualTo (2.0F));
+		Assert.That (Mathf.Clamp (-2.5F, -1.0F, 2.0F), Is.EqualTo (-1.0F));
+
+		// iregular args
+		// We shoul with named arg?
+		Assert.That (Mathf.Clamp (0.5F, 1.0F, -1.0F), Is.EqualTo (1.0F));
+		Assert.That (Mathf.Clamp (2.5F, 1.0F, -1.0F), Is.EqualTo (-1.0F));
+		Assert.That (Mathf.Clamp (-2.5F, 1.0F, -1.0F), Is.EqualTo (1.0F));
+	}
+
+
+	[Test]
 	public void MaxIntTest ()
 	{
 		Assert.That (Mathf.Max (1, 2), Is.EqualTo (2));
