245b04bf9d50be0543e2497e54eda0b0bcb28367
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-04-23 17:18:16 +0900

    added MoveTowardsAngle

--- Assets/Editor/MathfTest.cs
+++ Assets/Editor/MathfTest.cs
@@ -555,6 +555,38 @@ public class MathfTest
 	}
 
 	[Test]
+	public void MoveToWardsAngleTest ()
+	{
+		Assert.That (Mathf.MoveTowardsAngle (current: -5.0F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo (-4.0F));
+		Assert.That (Mathf.MoveTowardsAngle (current: -1.0F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo (0.0F));
+		Assert.That (Mathf.MoveTowardsAngle (current: -0.25F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo (0.0F));
+		Assert.That (Mathf.MoveTowardsAngle (current: 0.25F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo (0.0F));
+		Assert.That (Mathf.MoveTowardsAngle (current: 1.0F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo (0.0F));
+		Assert.That (Mathf.MoveTowardsAngle (current: 5.0F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo (4.0F));
+
+		Assert.That (Mathf.MoveTowardsAngle (current: -5.0F, target: 360.0F, maxDelta: 1.0F), Is.EqualTo (-4.0F));
+		Assert.That (Mathf.MoveTowardsAngle (current: -1.0F, target: 360.0F, maxDelta: 1.0F), Is.EqualTo (0.0F));
+		Assert.That (Mathf.MoveTowardsAngle (current: -0.25F, target: 360.0F, maxDelta: 1.0F), Is.EqualTo (0.0F));
+		Assert.That (Mathf.MoveTowardsAngle (current: 0.25F, target: 360.0F, maxDelta: 1.0F), Is.EqualTo (0.0F));
+		Assert.That (Mathf.MoveTowardsAngle (current: 1.0F, target: 360.0F, maxDelta: 1.0F), Is.EqualTo (0.0F));
+		Assert.That (Mathf.MoveTowardsAngle (current: 5.0F, target: 360.0F, maxDelta: 1.0F), Is.EqualTo (4.0F));
+
+		Assert.That (Mathf.MoveTowardsAngle (current: 355.0F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo (356.0F));
+		Assert.That (Mathf.MoveTowardsAngle (current: 359.0F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo (360.0F));
+		Assert.That (Mathf.MoveTowardsAngle (current: 359.75F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo (360.0F));
+		Assert.That (Mathf.MoveTowardsAngle (current: 360.25F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo (360.0F));
+		Assert.That (Mathf.MoveTowardsAngle (current: 361.0F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo (360.0F));
+		Assert.That (Mathf.MoveTowardsAngle (current: 365.0F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo (364.0F));
+
+		Assert.That (Mathf.MoveTowardsAngle (current: 355.0F, target: 360.0F, maxDelta: 1.0F), Is.EqualTo (356.0F));
+		Assert.That (Mathf.MoveTowardsAngle (current: 359.0F, target: 360.0F, maxDelta: 1.0F), Is.EqualTo (360.0F));
+		Assert.That (Mathf.MoveTowardsAngle (current: 359.75F, target: 360.0F, maxDelta: 1.0F), Is.EqualTo (360.0F));
+		Assert.That (Mathf.MoveTowardsAngle (current: 360.25F, target: 360.0F, maxDelta: 1.0F), Is.EqualTo (360.0F));
+		Assert.That (Mathf.MoveTowardsAngle (current: 361.0F, target: 360.0F, maxDelta: 1.0F), Is.EqualTo (360.0F));
+		Assert.That (Mathf.MoveTowardsAngle (current: 365.0F, target: 360.0F, maxDelta: 1.0F), Is.EqualTo (364.0F));
+	}
+
+	[Test]
 	public void PingPongTest ()
 	{
 		Assert.That (Mathf.PingPong (-1.75F, 1.0F), Is.EqualTo (0.25F));
