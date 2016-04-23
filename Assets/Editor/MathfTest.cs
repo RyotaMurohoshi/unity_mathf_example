6a0dcabcd62fcd36da7ace4e4b255cc673d42990
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-04-23 16:07:24 +0900

    added LinearToGammaSpaceTest

--- Assets/Editor/MathfTest.cs
+++ Assets/Editor/MathfTest.cs
@@ -354,6 +354,24 @@ public class MathfTest
 	}
 
 	[Test]
+	public void LinearToGammaSpaceTest ()
+	{
+		Assert.AreEqual (0.0F, Mathf.LinearToGammaSpace (0.0F));
+		Assert.AreEqual (0.3491902F, Mathf.LinearToGammaSpace (0.1F), 0.00001F);
+		Assert.AreEqual (0.4845292F, Mathf.LinearToGammaSpace (0.2F), 0.00001F);
+		Assert.AreEqual (0.5838314F, Mathf.LinearToGammaSpace (0.3F), 0.00001F);
+		Assert.AreEqual (0.6651850F, Mathf.LinearToGammaSpace (0.4F), 0.00001F);
+		Assert.AreEqual (0.7353570F, Mathf.LinearToGammaSpace (0.5F), 0.00001F);
+		Assert.AreEqual (0.7977377F, Mathf.LinearToGammaSpace (0.6F), 0.00001F);
+		Assert.AreEqual (0.8543057F, Mathf.LinearToGammaSpace (0.7F), 0.00001F);
+		Assert.AreEqual (0.9063317F, Mathf.LinearToGammaSpace (0.8F), 0.00001F);
+		Assert.AreEqual (0.9546872F, Mathf.LinearToGammaSpace (0.9F), 0.00001F);
+		Assert.AreEqual (1.0F, Mathf.LinearToGammaSpace (1.0F));
+
+		// TODO Add more test
+	}
+
+	[Test]
 	public void MaxIntTest ()
 	{
 		Assert.That (Mathf.Max (1, 2), Is.EqualTo (2));
