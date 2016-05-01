a489766d27530009d28e55f73376e0737ae59a1e
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-05-01 17:14:13 +0900

    added ToInt method

---
+++ Assets/Example/Scripts/TextAndSlider.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+public class TextAndSlider : MonoBehaviour
+{
+    [SerializeField]
+    Text text;
+    [SerializeField]
+    Slider slider;
+
+    void Awake()
+    {
+        slider.onValueChanged.AddListener(value => text.text = value.ToString());
+    }
+
+    public void AddListener(UnityAction<float> onValueChanged)
+    {
+        slider.onValueChanged.AddListener(onValueChanged);
+    }
+
+    public float value
+    {
+        set
+        {
+            slider.value = value;
+        }
+    }
+}
