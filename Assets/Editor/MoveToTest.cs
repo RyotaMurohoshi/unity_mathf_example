806c29a167bc872c39e27376eb8d3d7b84c7a23a
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-05-30 23:03:09 +0900

    added MoveToWards

--- Assets/Editor/MoveToTest.cs
+++ Assets/Editor/MoveToTest.cs
@@ -6,17 +6,47 @@ public class MoveToTest
     [Test]
     public void MoveToWardsTest()
     {
+        // http://docs.unity3d.com/ja/current/ScriptReference/Mathf.MoveTowards.html
         Assert.That(Mathf.MoveTowards(current: -5.0F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo(-4.0F));
         Assert.That(Mathf.MoveTowards(current: -1.0F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo(0.0F));
         Assert.That(Mathf.MoveTowards(current: -0.25F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo(0.0F));
+        Assert.That(Mathf.MoveTowards(current: 0.0F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo(0.0F));
         Assert.That(Mathf.MoveTowards(current: 0.25F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo(0.0F));
         Assert.That(Mathf.MoveTowards(current: 1.0F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo(0.0F));
         Assert.That(Mathf.MoveTowards(current: 5.0F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo(4.0F));
     }
 
     [Test]
+    public void MoveToWardsZeroTest()
+    {
+        // http://docs.unity3d.com/ja/current/ScriptReference/Mathf.MoveTowards.html
+        Assert.That(Mathf.MoveTowards(current: -5.0F, target: 0.0F, maxDelta: 0.0F), Is.EqualTo(-5.0F));
+        Assert.That(Mathf.MoveTowards(current: -1.0F, target: 0.0F, maxDelta: 0.0F), Is.EqualTo(-1.0F));
+        Assert.That(Mathf.MoveTowards(current: -0.25F, target: 0.0F, maxDelta: 0.0F), Is.EqualTo(-0.25F));
+        Assert.That(Mathf.MoveTowards(current: 0.0F, target: 0.0F, maxDelta: 0.0F), Is.EqualTo(0.0F));
+        Assert.That(Mathf.MoveTowards(current: 0.25F, target: 0.0F, maxDelta: 0.0F), Is.EqualTo(0.25F));
+        Assert.That(Mathf.MoveTowards(current: 1.0F, target: 0.0F, maxDelta: 0.0F), Is.EqualTo(1.0F));
+        Assert.That(Mathf.MoveTowards(current: 5.0F, target: 0.0F, maxDelta: 0.0F), Is.EqualTo(5.0F));
+    }
+
+    [Test]
+    public void MoveToWardsNegativeTest()
+    {
+        // http://docs.unity3d.com/ja/current/ScriptReference/Mathf.MoveTowards.html
+        Assert.That(Mathf.MoveTowards(current: -5.0F, target: 0.0F, maxDelta: -1.0F), Is.EqualTo(-6.0F));
+        Assert.That(Mathf.MoveTowards(current: -1.0F, target: 0.0F, maxDelta: -1.0F), Is.EqualTo(-2.0F));
+        Assert.That(Mathf.MoveTowards(current: -0.25F, target: 0.0F, maxDelta: -1.0F), Is.EqualTo(-1.25F));
+        Assert.That(Mathf.MoveTowards(current: 0.0F, target: 0.0F, maxDelta: -1.0F), Is.EqualTo(-1.0F));
+        Assert.That(Mathf.MoveTowards(current: 0.25F, target: 0.0F, maxDelta: -1.0F), Is.EqualTo(1.25F));
+        Assert.That(Mathf.MoveTowards(current: 1.0F, target: 0.0F, maxDelta: -1.0F), Is.EqualTo(2.0F));
+        Assert.That(Mathf.MoveTowards(current: 5.0F, target: 0.0F, maxDelta: -1.0F), Is.EqualTo(6.0F));
+    }
+
+
+    [Test]
     public void MoveToWardsAngleTest()
     {
+        // http://docs.unity3d.com/ja/current/ScriptReference/Mathf.MoveTowardsAngle.html
         Assert.That(Mathf.MoveTowardsAngle(current: -5.0F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo(-4.0F));
         Assert.That(Mathf.MoveTowardsAngle(current: -1.0F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo(0.0F));
         Assert.That(Mathf.MoveTowardsAngle(current: -0.25F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo(0.0F));
