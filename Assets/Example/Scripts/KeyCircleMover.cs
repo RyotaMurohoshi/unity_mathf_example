2b6c9e9af25ab181cec06c7d6377342f710493be
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-06-09 00:17:05 +0900

    added SmoothAngle Example

---
+++ Assets/Example/Scripts/KeyCircleMover.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class KeyCircleMover : MonoBehaviour
+{
+    public float Angle { get; private set; }
+
+    [SerializeField]
+    float radius = 1.0F;
+
+    [SerializeField]
+    Transform center;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            Angle += 5.0F * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            Angle -= 5.0F * Time.deltaTime;
+        }
+
+        transform.position = CalculatePosition(Angle);
+    }
+
+    public Vector3 CalculatePosition(float angle)
+    {
+        return center.transform.position + new Vector3(radius * Mathf.Cos(angle), radius * Mathf.Sin(angle), 0.0F);
+    }
+}
---
+++ Assets/Example/Scripts/SmoothAngleMathf.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class SmoothAngleMathf : MonoBehaviour
+{
+    float angle = 0.0F;
+    float velocity = 0.0F;
+
+    [SerializeField]
+    KeyCircleMover target;
+
+    [SerializeField]
+    float smoothTime = 0.1F;
+
+    void Update()
+    {
+        this.angle = Mathf.SmoothDampAngle(angle, target.Angle, ref velocity, smoothTime);
+        transform.position = target.CalculatePosition(this.angle);
+    }
+}
