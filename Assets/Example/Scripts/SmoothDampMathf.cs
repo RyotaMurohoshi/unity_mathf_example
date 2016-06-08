576ed1de59f4ffc283e0835fdd6ce13fda083c92
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-06-08 23:03:50 +0900

    added SmoothDampMathf

---
+++ Assets/Example/Scripts/SmoothDampMathf.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SmoothDampMathf : MonoBehaviour
+{
+    float velocity = 0.0F;
+
+    [SerializeField]
+    Transform target;
+
+    [SerializeField]
+    float smoothTime = 0.1F;
+
+    void Update()
+    {
+        float x = Mathf.SmoothDamp(
+            transform.position.x,
+            target.transform.position.x,
+            ref velocity,
+            smoothTime);
+
+        transform.position = new Vector3(x, transform.position.y, transform.position.z);
+    }
+}
