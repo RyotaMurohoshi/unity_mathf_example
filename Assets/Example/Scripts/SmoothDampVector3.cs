ee51b498e7da61b729146afda29d8e45047d861c
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-06-08 22:58:39 +0900

    added smoothdamp example

---
+++ Assets/Example/Scripts/SmoothDampVector3.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SmoothDampVector3 : MonoBehaviour
+{
+    Vector3 velocity = Vector3.zero;
+
+    [SerializeField]
+    Transform target;
+
+    [SerializeField]
+    float smoothTime = 0.1F;
+
+    void Update()
+    {
+        transform.position = Vector3.SmoothDamp(
+            transform.position,
+            target.transform.position,
+            ref velocity,
+            smoothTime);
+    }
+}
