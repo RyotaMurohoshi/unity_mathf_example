71ecd3eca368b139dda68d07b75c8fd0fab6c0c9
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-10-30 14:51:47 +0900

    fix test

--- Assets/Editor/MoveToTest.cs
+++ Assets/Editor/MoveToTest.cs
@@ -56,15 +56,15 @@ public class MoveToTest
 
         Assert.That(Mathf.MoveTowardsAngle(current: -5.0F, target: 360.0F, maxDelta: 1.0F), Is.EqualTo(-4.0F));
         Assert.That(Mathf.MoveTowardsAngle(current: -1.0F, target: 360.0F, maxDelta: 1.0F), Is.EqualTo(0.0F));
-        Assert.That(Mathf.MoveTowardsAngle(current: -0.25F, target: 360.0F, maxDelta: 1.0F), Is.EqualTo(0.0F));
-        Assert.That(Mathf.MoveTowardsAngle(current: 0.25F, target: 360.0F, maxDelta: 1.0F), Is.EqualTo(0.0F));
+        Assert.That(Mathf.MoveTowardsAngle(current: -0.25F, target: 360.0F, maxDelta: 1.0F), Is.EqualTo(360.0F));
+        Assert.That(Mathf.MoveTowardsAngle(current: 0.25F, target: 360.0F, maxDelta: 1.0F), Is.EqualTo(360.0F));
         Assert.That(Mathf.MoveTowardsAngle(current: 1.0F, target: 360.0F, maxDelta: 1.0F), Is.EqualTo(0.0F));
         Assert.That(Mathf.MoveTowardsAngle(current: 5.0F, target: 360.0F, maxDelta: 1.0F), Is.EqualTo(4.0F));
 
         Assert.That(Mathf.MoveTowardsAngle(current: 355.0F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo(356.0F));
         Assert.That(Mathf.MoveTowardsAngle(current: 359.0F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo(360.0F));
-        Assert.That(Mathf.MoveTowardsAngle(current: 359.75F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo(360.0F));
-        Assert.That(Mathf.MoveTowardsAngle(current: 360.25F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo(360.0F));
+        Assert.That(Mathf.MoveTowardsAngle(current: 359.75F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo(0.0F));
+        Assert.That(Mathf.MoveTowardsAngle(current: 360.25F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo(0.0F));
         Assert.That(Mathf.MoveTowardsAngle(current: 361.0F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo(360.0F));
         Assert.That(Mathf.MoveTowardsAngle(current: 365.0F, target: 0.0F, maxDelta: 1.0F), Is.EqualTo(364.0F));
 
